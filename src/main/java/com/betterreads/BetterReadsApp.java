c628c07071f3edfdbc183865113b79916fe251f8
Ravi Ranjan <raviranjan3570@gmail.com>
2022-01-24 22:34:54 +0530

    added BookController and established a connection with cassandra

--- src/main/java/com/betterreads/BetterReadsApp.java
+++ src/main/java/com/betterreads/BetterReadsApp.java
@@ -1,7 +1,14 @@
 package com.betterreads;
 
+import java.nio.file.Path;
+
+import com.betterreads.connection.DataStaxAstraProperties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterReadsApp {
 
 	public static void main(String[] args) {
@@ -21,4 +29,10 @@ public class BetterReadsApp {
 		return principal.getAttribute("name");
 	}
 
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(
+			DataStaxAstraProperties properties) {
+		Path bundle = properties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
 }
