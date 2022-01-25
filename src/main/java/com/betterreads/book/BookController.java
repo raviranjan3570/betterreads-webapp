85dc8d835d1cbd9a2f1caa30548b7449ff2125d1
Ravi Ranjan <raviranjan3570@gmail.com>
2022-01-25 22:30:01 +0530

    added a simple book view

--- src/main/java/com/betterreads/book/BookController.java
+++ src/main/java/com/betterreads/book/BookController.java
@@ -14,11 +14,18 @@ public class BookController {
     @Autowired
     private BookRepository bookRepository;
 
+    private final String COVER_IMAGE_ROOT = "http://covers.openlibrary.org/b/id/";
+
     @GetMapping(value = "/books/{bookId}")
     public String getBook(@PathVariable String bookId, Model model) {
         Optional<Book> optionalBook = bookRepository.findById(bookId);
         if (optionalBook.isPresent()) {
             Book book = optionalBook.get();
+            String coverImageUrl = "/images/no-image.png";
+            if (book.getCoverIds() != null && book.getCoverIds().size() > 0) {
+                coverImageUrl = COVER_IMAGE_ROOT + book.getCoverIds().get(0) + "-L.jpg";
+            }
+            model.addAttribute("coverImage", coverImageUrl);
             model.addAttribute("book", book);
             return "book";
         }
