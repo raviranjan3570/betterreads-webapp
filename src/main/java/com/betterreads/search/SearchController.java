0ec3297a0285d9c75131ec50305473cfd6494bc3
Ravi Ranjan <raviranjan3570@gmail.com>
2022-01-27 19:53:36 +0530

    added search functionality

--- src/main/java/com/betterreads/search/SearchController.java
+++ src/main/java/com/betterreads/search/SearchController.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
@@ -14,6 +15,8 @@ import reactor.core.publisher.Mono;
 
 @Controller
 public class SearchController {
+
+        private final String COVER_IMAGE_ROOT = "http://covers.openlibrary.org/b/id/";
 
         private final WebClient webClient;
 
@@ -36,6 +39,17 @@ public class SearchController {
                 List<SearchResultBook> books = result.getDocs()
                                 .stream()
                                 .limit(10)
+                                .map(bookResult -> {
+                                        bookResult.setKey(bookResult.getKey().replace("/works/", ""));
+                                        String coverId = bookResult.getCover_i();
+                                        if (StringUtils.hasText(coverId)) {
+                                                coverId = COVER_IMAGE_ROOT + coverId + "-M.jpg";
+                                        } else {
+                                                coverId = "/images/no-image.png";
+                                        }
+                                        bookResult.setCover_i(coverId);
+                                        return bookResult;
+                                })
                                 .collect(Collectors.toList());
                 model.addAttribute("searchResult", books);
                 return "search";
